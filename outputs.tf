59a816ee878ce4eabfea76826d2797ed4b605426
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:20:36 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,39 +1,34 @@
-output "spring_cloud_customized_accelerators" {
-  description = "All spring_cloud_customized_accelerator resources"
-  value       = azurerm_spring_cloud_customized_accelerator.spring_cloud_customized_accelerators
-  sensitive   = true
-}
 output "spring_cloud_customized_accelerators_accelerator_tags" {
-  description = "List of accelerator_tags values across all spring_cloud_customized_accelerators"
-  value       = [for k, v in azurerm_spring_cloud_customized_accelerator.spring_cloud_customized_accelerators : v.accelerator_tags]
+  description = "Map of accelerator_tags values across all spring_cloud_customized_accelerators, keyed the same as var.spring_cloud_customized_accelerators"
+  value       = { for k, v in azurerm_spring_cloud_customized_accelerator.spring_cloud_customized_accelerators : k => v.accelerator_tags }
 }
 output "spring_cloud_customized_accelerators_accelerator_type" {
-  description = "List of accelerator_type values across all spring_cloud_customized_accelerators"
-  value       = [for k, v in azurerm_spring_cloud_customized_accelerator.spring_cloud_customized_accelerators : v.accelerator_type]
+  description = "Map of accelerator_type values across all spring_cloud_customized_accelerators, keyed the same as var.spring_cloud_customized_accelerators"
+  value       = { for k, v in azurerm_spring_cloud_customized_accelerator.spring_cloud_customized_accelerators : k => v.accelerator_type }
 }
 output "spring_cloud_customized_accelerators_description" {
-  description = "List of description values across all spring_cloud_customized_accelerators"
-  value       = [for k, v in azurerm_spring_cloud_customized_accelerator.spring_cloud_customized_accelerators : v.description]
+  description = "Map of description values across all spring_cloud_customized_accelerators, keyed the same as var.spring_cloud_customized_accelerators"
+  value       = { for k, v in azurerm_spring_cloud_customized_accelerator.spring_cloud_customized_accelerators : k => v.description }
 }
 output "spring_cloud_customized_accelerators_display_name" {
-  description = "List of display_name values across all spring_cloud_customized_accelerators"
-  value       = [for k, v in azurerm_spring_cloud_customized_accelerator.spring_cloud_customized_accelerators : v.display_name]
+  description = "Map of display_name values across all spring_cloud_customized_accelerators, keyed the same as var.spring_cloud_customized_accelerators"
+  value       = { for k, v in azurerm_spring_cloud_customized_accelerator.spring_cloud_customized_accelerators : k => v.display_name }
 }
 output "spring_cloud_customized_accelerators_git_repository" {
-  description = "List of git_repository values across all spring_cloud_customized_accelerators"
-  value       = [for k, v in azurerm_spring_cloud_customized_accelerator.spring_cloud_customized_accelerators : v.git_repository]
+  description = "Map of git_repository values across all spring_cloud_customized_accelerators, keyed the same as var.spring_cloud_customized_accelerators"
+  value       = { for k, v in azurerm_spring_cloud_customized_accelerator.spring_cloud_customized_accelerators : k => v.git_repository }
   sensitive   = true
 }
 output "spring_cloud_customized_accelerators_icon_url" {
-  description = "List of icon_url values across all spring_cloud_customized_accelerators"
-  value       = [for k, v in azurerm_spring_cloud_customized_accelerator.spring_cloud_customized_accelerators : v.icon_url]
+  description = "Map of icon_url values across all spring_cloud_customized_accelerators, keyed the same as var.spring_cloud_customized_accelerators"
+  value       = { for k, v in azurerm_spring_cloud_customized_accelerator.spring_cloud_customized_accelerators : k => v.icon_url }
 }
 output "spring_cloud_customized_accelerators_name" {
-  description = "List of name values across all spring_cloud_customized_accelerators"
-  value       = [for k, v in azurerm_spring_cloud_customized_accelerator.spring_cloud_customized_accelerators : v.name]
+  description = "Map of name values across all spring_cloud_customized_accelerators, keyed the same as var.spring_cloud_customized_accelerators"
+  value       = { for k, v in azurerm_spring_cloud_customized_accelerator.spring_cloud_customized_accelerators : k => v.name }
 }
 output "spring_cloud_customized_accelerators_spring_cloud_accelerator_id" {
-  description = "List of spring_cloud_accelerator_id values across all spring_cloud_customized_accelerators"
-  value       = [for k, v in azurerm_spring_cloud_customized_accelerator.spring_cloud_customized_accelerators : v.spring_cloud_accelerator_id]
+  description = "Map of spring_cloud_accelerator_id values across all spring_cloud_customized_accelerators, keyed the same as var.spring_cloud_customized_accelerators"
+  value       = { for k, v in azurerm_spring_cloud_customized_accelerator.spring_cloud_customized_accelerators : k => v.spring_cloud_accelerator_id }
 }
 
